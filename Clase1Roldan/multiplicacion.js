acdb08d9e582a82c6e4ef5c41383dfd20455d73b
Federico <roldanfede96@gmail.com>
2021-12-21 09:11:49 -0300

    Clase1

---
+++ Clase1Roldan/multiplicacion.js
@@ -0,0 +1,8 @@
+console.log("¡Hola Fede!");
+let primerValor = Number(prompt("Ingrese el primer valor que desea multiplicar."));
+let segundoValor = Number(prompt("Ingrese el segundo valor que desea multiplicar."));
+let resultado = primerValor * segundoValor;
+confirm("El resultado se puede ver en consola!");
+console.log("El resultado de la multiplicacion es:")
+console.log(primerValor + " * " + segundoValor + " = " + resultado);
+
