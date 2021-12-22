d11f08f95aa46fdcd2ab60dcb49441617046bb65
Federico <roldanfede96@gmail.com>
2021-12-22 18:45:11 -0300

    acomodando tp clase2

--- Clase2Roldan/jubilacion.js
+++ Clase2Roldan/jubilacion.js
@@ -1,13 +1,20 @@
-console.log("¡Hola Fede! Para saber que tipo de Jubilacion le corresponde, ingrese su edad y sus años de antiguedad.");
+
+/*Existen tres tipos de jubilaciones: por edad, por antigüedad anticipada y por antigüedad y edad adulta:
+1.- Las personas con jubilación por edad deben tener 60 años o más y una antigüedad en su empleo de menos de 25 años.
+2.- Las personas adscritas a la jubilación por antigüedad anticipada deben tener menos de 60 años y una antigüedad en su empleo de 25 años o más.
+3.- Las personas adscritas a la jubilación por antigüedad adulta deben tener 60 años o más y una antigüedad en su empleo de 25 años o más.
+*/
+
+console.log("¡Hola Fede! Para saber que tipo de Jubilacion le corresponde, ingrese su edad y sus años de antigüedad.");
 let edad = parseInt(prompt("Ingrese su Edad."));
-let antiguedad = parseInt(prompt("Ingrese cuantos años de antiguedad tiene."));
-confirm("El resultado se puede ver en consola!");
+let antiguedad = parseInt(prompt("Ingrese cuantos años de antigüedad tiene."));
+confirm("!El resultado se puede ver en consola!");
 if(edad >= 60 && antiguedad < 25){
-    console.log("Con una edad de " + edad + " y con una antiguedad de " + antiguedad + ", te corresponde una Jubilacion por edad."); 
+    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", te corresponde una Jubilacion por edad."); 
 }else if(edad < 60 && antiguedad >= 25){
-    console.log("Con una edad de " + edad + " y con una antiguedad de " + antiguedad + ", te corresponde una Jubilacion anticipada."); 
+    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", te corresponde una Jubilacion por antigüedad anticipada."); 
 }else if(edad >= 60 && antiguedad >= 25){
-    console.log("Con una edad de " + edad + " y con una antiguedad de " + antiguedad + ", te corresponde una Jubilacion ordinaria.") 
+    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", te corresponde una Jubilacion por antiguedad y edad adulta.") 
 }else{
-    console.log("Con una edad de " + edad + " y con una antiguedad de " + antiguedad + ", aun no  te corresponde una jubilacion.") 
+    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", aun no  te corresponde una jubilacion.") 
 }
