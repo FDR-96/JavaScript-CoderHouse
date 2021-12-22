8818a852a4538a858c768551f9e93ab1da7c3e72
Federico <roldanfede96@gmail.com>
2021-12-22 18:48:14 -0300

    arreglando errores de ortografia

--- Clase2Roldan/jubilacion.js
+++ Clase2Roldan/jubilacion.js
@@ -1,4 +1,3 @@
-
 /*Existen tres tipos de jubilaciones: por edad, por antigüedad anticipada y por antigüedad y edad adulta:
 1.- Las personas con jubilación por edad deben tener 60 años o más y una antigüedad en su empleo de menos de 25 años.
 2.- Las personas adscritas a la jubilación por antigüedad anticipada deben tener menos de 60 años y una antigüedad en su empleo de 25 años o más.
@@ -10,11 +9,11 @@ let edad = parseInt(prompt("Ingrese su Edad."));
 let antiguedad = parseInt(prompt("Ingrese cuantos años de antigüedad tiene."));
 confirm("!El resultado se puede ver en consola!");
 if(edad >= 60 && antiguedad < 25){
-    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", te corresponde una Jubilacion por edad."); 
+    console.log("Con una edad de " + edad + " años y con una antigüedad de " + antiguedad + "años, te corresponde una Jubilacion por edad."); 
 }else if(edad < 60 && antiguedad >= 25){
-    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", te corresponde una Jubilacion por antigüedad anticipada."); 
+    console.log("Con una edad de " + edad + "años y con una antigüedad de " + antiguedad + "años, te corresponde una Jubilacion por antigüedad anticipada."); 
 }else if(edad >= 60 && antiguedad >= 25){
-    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", te corresponde una Jubilacion por antiguedad y edad adulta.") 
+    console.log("Con una edad de " + edad + "años y con una antigüedad de " + antiguedad + "años, te corresponde una Jubilacion por antiguedad y edad adulta.") 
 }else{
-    console.log("Con una edad de " + edad + " y con una antigüedad de " + antiguedad + ", aun no  te corresponde una jubilacion.") 
+    console.log("Con una edad de " + edad + "años y con una antigüedad de " + antiguedad + "años, aun no  te corresponde una jubilacion.") 
 }
