d1f3b8f2f351c379798a993d06b423eaa6942016
Federico <roldanfede96@gmail.com>
2022-01-17 13:08:49 -0300

    Ordenando Objetos

---
+++ Clase6Roldan/OrdenarArrayDeObjetos-RoldanFederico/index.js
@@ -0,0 +1,97 @@
+/* 
+Autor: Federico Roldan.
+Clase: 6.
+Desafio: Ordenar Arrays de Objetos.
+Informacion sobre el ejercicio: Calcula cuanto vas a gastar si realizas compras en el exterior o por internet desde Argentina.
+- Calcular el importe para el impuesto Aduanero del 50%.
+- Calcular el importe para el impuesto Dolar Turista del 30%.
+- Realizar la conversión total USD -> ARS con los impuestos incluidos.
+ */
+
+
+class Producto {
+    constructor(description, amount, weight) {
+        this.description = description;
+        this.weight = weight;
+        this.amount = amount
+    }
+    calcular () {
+        let [ars, impuestoAduanero, impuestoDolarTurista, total] = CalcularImpuestosProductos(this.amount);
+        return [ars, impuestoAduanero, impuestoDolarTurista, total]
+    }
+} 
+
+
+const CalcularImpuestosProductos = (usd) =>{
+    let ars = DolarAPesos(usd);
+    let impuestoAduanero = parseFloat(ars * 0.5);
+    let impuestoDolarTurista = parseFloat(ars * 0.3);
+    let total = impuestoAduanero + impuestoDolarTurista + ars;
+    return [ars, impuestoAduanero, impuestoDolarTurista, total]
+}
+
+const DolarAPesos = (usd) =>{
+    return parseFloat(usd * 102)
+}
+
+
+
+let arrayProducto = []; 
+
+/* 
+Utilice un cliclo "do" ya que con un "while" y un "for" no encontre la forma de utilizar el confirm sin que quedara fuera del ciclo y este
+me sirviera de "bandera" para añadir productos y romper el ciclo. 
+Siempre que el confirm retorne un true, se añadira un nuevo ojeto y preguntara si se desea añadir uno nuevo.
+*/
+do{
+    var result = confirm("Fede ¿Deseas añadir nuevos productos?");
+    if(result){
+        let description = prompt("Descripcion del Producto");
+        let amount = prompt("Monto en dolares");
+        let weight = prompt("Peso en Kg del producto");
+        arrayProducto.push(new Producto(description, amount, weight));
+    }
+}
+while(result);
+
+/* 
+Recorremos el array y mostramos todos los datos de los productos
+*/
+
+for (let producto of arrayProducto){
+
+    console.log(producto.description + " Monto: " + producto.amount + "U$D Peso: ", producto.weight + "Kg");
+    
+    let [ars, impuestoAduanero, impuestoDolarTurista, total] = producto.calcular();
+    console.log( "Valor en Pesos: " + ars + "AR$");
+    console.log( "Impuesto Aduanero: " + impuestoAduanero + "AR$"); 
+    console.log( "Impuesto Dolar Turista: " + impuestoDolarTurista + "AR$");
+    console.log( "Total: " + total + "AR$")
+    
+    
+    document.write("<br>");
+    document.write("<H2>Descripcion: " + producto.description + "</H2>");
+    document.write("<H3>Monto: " + producto.amount + "U$D</H3>");
+    document.write("<H3>Peso: " + producto.weight + "KG</H3>");
+    document.write("<H3>Monto en pesos: " + ars + "AR$</H3>");
+    document.write("<H3>Impuesto Aduanero: " + impuestoAduanero + "AR$</H3>");
+    document.write("<H3>Impuesto Dolar Turista: " + impuestoDolarTurista + "AR$</H3>");
+    document.write("<H3>Total a pagar: " + total + "AR$</H3>"); 
+
+   
+}
+
+/* 
+Ordenamos de mayor a menos los montos. 
+*/
+
+let ordenadosPorMonto = [];
+ordenadosPorMonto = arrayProducto.map(elemento => elemento);
+ordenadosPorMonto = arrayProducto;
+ordenadosPorMonto.sort(function(a, b){
+    return b.amount - a.amount;
+});
+for (let producto of ordenadosPorMonto){
+    console.log("Productos Ordenados: ")
+    console.log(producto.description.toUpperCase() + " Monto: " + producto.amount + "U$D Peso: " + producto.weight + "Kg Cantidad:");
+}
---
+++ Clase6Roldan/PreEntrega-ProtectoFinal-RoldanFederico/index.js
@@ -0,0 +1,115 @@
+/* 
+# Autor: Federico Roldan.
+# Proyecto Final Primera Entrega: Servicio Curier Condor.
+# Informacion sobre el Proyecto: 
+        En mi proyecto se cotiza una importacion con un servicio curier ficticio llamado "CONDOR", se podran ir
+    sumando productos y se mostrara detalladamente los costos que se van a tener, dependiendo de las variables ingresadas. 
+
+    Se calcula cuanto vas a gastar si realizas compras en el exterior o por internet desde Argentina.   
+    - Calcular el importe para el impuesto Aduanero del 50%.
+    - Calcular el importe para el impuesto Dolar Turista del 30%.
+    - Realizar la conversión total USD -> ARS con los impuestos incluidos.
+    - Calcula el valor del envio según el peso del Producto.
+ */
+
+let arrayProducto = []; //Array dinamico donde se almacenan los productos que se van creando.
+const transport = 22; //El precio en  dolares de lo que sale el transporte en relacion a 1 Kg. El precio del envio sera flete * weight. 
+
+/* 
+Creamos el objeto Producto con sus propiedades y funcionalidad. Tambien añadimos un constructor para crear nuevos productos cuando
+el usuario lo requiera.
+La funcion calcular transformara divisas, calculara impuestos, transpoerte y retoranara esos valores para luego ser visualizados. 
+*/
+class Producto {
+    constructor(description, amount, weight, quantity) {
+        this.description = description;
+        this.weight = weight;
+        this.amount = amount;
+        this.quantity = quantity;
+    }
+    calcular(){
+        let [ars, impuestoAduanero, impuestoDolarTurista, shipping, total] = CalcularValorFinal(this.amount, this.weight, this.quantity);
+        return [ars, impuestoAduanero, impuestoDolarTurista, shipping, total];
+    }
+} 
+
+//Se llama a las demas funciones y se calcula el total, esta funcion retornara todos los valores conversidos en pesos argentinos.
+const CalcularValorFinal = (usd, weight, quantity) =>{
+    let ars = DolarAPesos(usd);
+    let shipping = DolarAPesos(CalcularEnvio(weight * quantity));  //CalcularEnvio devulve el valor en dolares por lo que es necesario transformar a pesos
+    let [impuestoAduanero, impuestoDolarTurista] = CalcularImpuestosProductos(ars * quantity);
+    let total = impuestoAduanero + impuestoDolarTurista + ars + shipping;
+    return [ars, impuestoAduanero, impuestoDolarTurista, shipping, total]
+}
+
+const CalcularImpuestosProductos = (ars) =>{
+    let impuestoAduanero = parseFloat(ars * 0.5);
+    let impuestoDolarTurista = parseFloat(ars * 0.3);
+    return [impuestoAduanero, impuestoDolarTurista]
+}
+
+const CalcularEnvio = (weight) =>{
+    let shipping = transport * weight; //transport en U$D por ende shipping en U$D
+    return shipping
+}
+
+const DolarAPesos = (usd) =>{
+    return parseFloat(usd * 102) //En esta etapa del proyecto el valor que le asigne al dolar no se modifica, pero me gustaria tomarlo con una API.
+}
+
+/* 
+Utilice un ciclo "do" ya que con un "while" y un "for" no encontre la forma de utilizar el confirm sin que quedara fuera del ciclo y este
+me sirviera de "bandera" para añadir productos y romper el ciclo. 
+Siempre que el confirm retorne un true, se añadira un nuevo ojeto y preguntara si se desea añadir uno nuevo.
+*/
+do{
+    //Preguntamos si se quiere añadir un producto.
+    var result = confirm("Fede ¿Deseas añadir nuevos productos?");
+    if(result){
+        //Pedimos los datos del producto.
+        let description = prompt("Descripcion del Producto");
+        let amount = prompt("Monto en dolares");
+        let weight = prompt("Peso en Kg del producto");
+        let quantity = prompt("Cantidad");
+        //Añadimos con el metodo push y creamos un nuevo producto al cual le pasamos los parametros necesarios.
+        arrayProducto.push(new Producto(description, amount, weight, quantity));
+    }
+}
+while(result);
+
+
+
+let ordenadosPorMonto = [];
+ordenadosPorMonto = arrayProducto.map(elemento => elemento);
+ordenadosPorMonto = arrayProducto;
+ordenadosPorMonto.sort(function(a, b){
+    return b.amount - a.amount;
+});
+
+// Recorremos el array y mostramos todos los datos de los productos en nuestra pagina.
+for (let producto of ordenadosPorMonto){
+   
+    //Llamamos a la funcion calcular() para que realice las operaciones necesarias y mostramos los datos obtenidos.
+    let [ars, impuestoAduanero, impuestoDolarTurista, shipping, total] = producto.calcular();
+    
+
+    console.log(producto.description.toUpperCase() + " Monto: " + producto.amount + "U$D Peso: " + producto.weight + "Kg Cantidad:", producto.quantity + " Unidades ");
+    console.log( "Valor en Pesos: " + ars + "AR$");
+    console.log( "Impuesto Aduanero: " + impuestoAduanero + "AR$"); 
+    console.log( "Impuesto Dolar Turista: " + impuestoDolarTurista + "AR$");
+    console.log( "Envio: " + shipping + "AR$");
+    console.log( "Total: " + total + "AR$")
+    
+
+    document.write("<br>");
+    document.write("<h2>Descripcion: " + producto.description.toUpperCase() + "</h2>");
+    document.write("<h3>Monto: </span>" + producto.amount + "U$D</span></h3>");
+    document.write("<h3>Peso: </span>" + producto.weight + "KG</span></h3>");
+    document.write("<h3>Cantidad: </span>" + producto.quantity + "</span></h3>");
+    document.write("<h3>Monto en pesos: <span>" + ars + "AR$</span></h3>");
+    document.write("<h3>Impuesto Aduanero: <span>" + impuestoAduanero + "AR$</span></h3>");
+    document.write("<h3>Impuesto Dolar Turista: <span>" + impuestoDolarTurista + "AR$</span></h3>");
+    document.write("<h3>Envio: <span>" + shipping + "AR$</h3>");
+    document.write("<h3>Total a pagar:  " + total + "AR$</h3>");
+}
+
