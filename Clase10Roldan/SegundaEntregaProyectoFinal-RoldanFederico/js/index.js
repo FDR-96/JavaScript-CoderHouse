ee0953da8bc63d311d987807d09ac4159f89cbf3
Federico <roldanfede96@gmail.com>
2022-02-13 09:22:31 -0300

    segunda entrega proyecto final

--- Clase10Roldan/SegundaEntregaProyectoFinal-RoldanFederico/js/index.js
+++ Clase10Roldan/SegundaEntregaProyectoFinal-RoldanFederico/js/index.js
@@ -1,6 +1,6 @@
 /* 
 # Autor: Federico Roldan.
-# Ejercicio: Incorporar Eventos.
+# Segunda Entrega Proyecto Final
 # Informacion sobre el Proyecto: 
         En mi proyecto se cotiza una importacion con un servicio curier ficticio llamado "CONDOR", se podran ir
     sumando productos y se mostrara detalladamente los costos que se van a tener, dependiendo de las variables ingresadas. 
@@ -33,14 +33,6 @@ class Producto {
         console.log(this.description, this.amount + "U$D", this.weight + "Kg", this.quantity + " Unidades ");
         console.log(ars, impuestoAduanero, impuestoDolarTurista, shipping, total);
         return [ars, impuestoAduanero, impuestoDolarTurista, shipping, total];
-    }
-    eliminar(){
-        delete this.key;
-        delete this.descripcion;
-        delete this.weight;
-        delete this.amount;
-        delete this.quantity;
-        return;
     }
 } 
 
